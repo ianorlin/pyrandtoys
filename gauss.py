76d2386bfa9e61ac17bca396384772ae70fb4563
Brendan Perrine <walterorlin@gmail.com>
2015-09-26 12:19:52 -0700

    Add one liner to add ability to print a normal distribution with mean zero and varience one

---
+++ gauss.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3 
+
+#Copyright 2015 BRendan Perrine
+
+import random 
+
+random.seed()
+
+print (random.gauss(0,1), "Is a normal distribution with mean zero and standard deviation and varience of one")
